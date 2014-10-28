9069b03f054e5783def63a68191dda39cb184f14
torito1984 <in.the.gomera@gmail.com>
2014-10-28 21:25:35 +0100

    first commit

---
+++ src/main/java/uk/ucl/Main.java
@@ -0,0 +1,11 @@
+package uk.ucl;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        System.out.println(RandomStringUtils.randomAlphabetic(10));
+    }
+}
